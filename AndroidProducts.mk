a9c5e720a17381f66f7921478cbba137dff59af8
niexia11 <luqasy0211021111@gmail.com>
2026-06-10 15:56:33 +0800

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,10 +1,2 @@
-#
-# Copyright (C) 2026 The Android Open Source Project
-# Copyright (C) 2026 TWRP Open Source Project
-#
-
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/twrp_X6840B.mk
-
-COMMON_LUNCH_CHOICES := \
-    twrp_X6840B-trunk_staging-eng
